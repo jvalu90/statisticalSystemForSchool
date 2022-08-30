7a20ed63afce08bc7414b0e9b411a62584ef9625
Jimmy Valencia Urbano <jvalu90@gmail.com>
2022-08-29 20:07:14 -0500

    Algorithm's Structure

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+      try {
+
+        } catch (Exception ex) {
+
+        } finally {
+
+        }
     }
 }
